60b1f375762a83bdf410da6246dc3d582960cab0
Edmund Huber <edmund@fastly.com>
2014-05-09 22:57:57 +0200

    setup.py cleanup and classifiers, fixes #11

--- setup.py
+++ setup.py
@@ -5,22 +5,23 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "fastly",
-    version = "0.0.2",
-    author = "Fastly?",
+    name="fastly",
+    version="0.0.2",
+    author="Fastly",
     author_email="support@fastly.com",
-    description = ("Fastly python API"),
-    license = "dunno",
-    keywords = "fastly api",
-    url = "https://github.com/fastly/fastly-py",
+    description="Fastly python API",
+    keywords="fastly api",
+    url="https://github.com/fastly/fastly-py",
     packages=find_packages(),
     long_description=read('README'),
     classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
         "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved??? :: BSD License???",
+        "Topic :: Utilities"
     ],
-    entry_points = {
+    entry_points={
       'console_scripts': [
         'purge_service = fastly.scripts.purge_service:purge_service',
         'purge_key     = fastly.scripts.purge_key:purge_key',
