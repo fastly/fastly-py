b0dd18d4e4e18dafae9d93848f633afc396c91b4
Edmund Huber <edmund@fastly.com>
2014-05-19 20:16:34 +0200

    remove outdated/misguided meta __variables__, https://mail.python.org/pipermail/python-dev/2001-March/013328.html

--- fastly/__init__.py
+++ fastly/__init__.py
@@ -1,11 +1,1 @@
-"""
-
-"""
-
 from fastly import *
-
-__author__ = 'Tyler McMullen <tbmcmullen@gmail.com>'
-__copyright__ = 'Copyright (c) 2012 Fastly Inc'
-__license__ = 'BSD'
-__version__ = '0.0.1'
-__url__ = 'http://www.fastly.com/docs/fastly-py'
