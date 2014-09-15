1bc048d1c7890aa294e9fa9aa8a5a560635f72d9
Thom Mahoney <mahoneyt@gmail.com>
2014-09-15 10:49:11 -0700

    require key authentication for purge_by_key

--- test/api_test.py
+++ test/api_test.py
@@ -11,6 +11,17 @@ class APITest(unittest.TestCase):
     def test_purge(self):
         self.assertTrue(self.api.purge_url('test.com', '/'))
 
+    def test_purge_by_key(self):
+        self.api.deauthenticate()
+        self.api.authenticate_by_key('TESTAPIKEY')
+        self.assertTrue(self.api.purge_key('test.com', 'foo'))
+
+    def test_cookie_purge_by_key(self):
+        self.api.deauthenticate()
+        self.api.authenticate_by_password('foo@example.com', 'password')
+        with self.assertRaises(fastly.AuthenticationError):
+            self.api.purge_key('test.com', 'foo')
+
     def test_auth_error(self):
         self.api.deauthenticate()
         with self.assertRaises(fastly.AuthenticationError):
