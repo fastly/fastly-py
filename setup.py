6b37029472d562a386283dad042c189add331574
blithe <blitherocher@gmail.com>
2016-05-10 00:01:37 +0000

    Bump version to 0.1.0 to indicate additional functionality

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="fastly",
-    version="0.0.5",
+    version="0.1.0",
     author="Fastly",
     author_email="support@fastly.com",
     description="Fastly python API",
