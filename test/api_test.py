b67ac1eb0cd063a24b67886ab73cb88ef5c563f6
blithe <blitherocher@gmail.com>
2016-05-10 22:02:38 +0000

    Allow user to pass optional environment variables to tests
    * Defaults to current values if env variables are not set

--- test/api_test.py
+++ test/api_test.py
@@ -1,5 +1,5 @@
 import unittest
-
+import os
 import fastly
 
 
@@ -7,35 +7,40 @@ class APITest(unittest.TestCase):
 
     def setUp(self):
         self.api = fastly.API()
+        self.host = os.environ.get('FASTLY_SERVICE_HOST', 'test.com')
+        self.service_id = os.environ.get('FASTLY_SERVICE_ID', 'test.com')
+        self.api_key = os.environ.get('FASTLY_API_KEY', 'TESTAPIKEY')
+        self.user = os.environ.get('FASTLY_USER', 'foo@example.com')
+        self.password = os.environ.get('FASTLY_PASSWORD', 'password')
 
     def test_purge(self):
-        self.assertTrue(self.api.purge_url('test.com', '/'))
+        self.assertTrue(self.api.purge_url(self.host, '/'))
 
     def test_purge_by_key(self):
         self.api.deauthenticate()
-        self.api.authenticate_by_key('TESTAPIKEY')
-        self.assertTrue(self.api.purge_key('test.com', 'foo'))
+        self.api.authenticate_by_key(self.api_key)
+        self.assertTrue(self.api.purge_key(self.service_id, 'foo'))
 
     def test_cookie_purge_by_key(self):
         self.api.deauthenticate()
-        self.api.authenticate_by_password('foo@example.com', 'password')
+        self.api.authenticate_by_password(self.user, self.password)
         with self.assertRaises(fastly.AuthenticationError):
-            self.api.purge_key('test.com', 'foo')
+            self.api.purge_key(self.service_id, 'foo')
 
     def test_soft_purge(self):
         self.api.deauthenticate()
-        self.assertTrue(self.api.soft_purge_url('test.com', '/'))
+        self.assertTrue(self.api.soft_purge_url(self.host, '/'))
 
     def test_soft_purge_by_key(self):
         self.api.deauthenticate()
-        self.api.authenticate_by_key('TESTAPIKEY')
-        self.assertTrue(self.api.soft_purge_key('test.com', 'foo'))
+        self.api.authenticate_by_key(self.api_key)
+        self.assertTrue(self.api.soft_purge_key(self.service_id, 'foo'))
 
     def test_cookie_soft_purge_by_key(self):
         self.api.deauthenticate()
-        self.api.authenticate_by_password('foo@example.com', 'password')
+        self.api.authenticate_by_password(self.user, self.password)
         with self.assertRaises(fastly.AuthenticationError):
-            self.api.soft_purge_key('test.com', 'foo')
+            self.api.soft_purge_key(self.service_id, 'foo')
 
     def test_auth_error(self):
         self.api.deauthenticate()
@@ -44,12 +49,12 @@ class APITest(unittest.TestCase):
 
     def test_auth_key_success(self):
         self.api.deauthenticate()
-        self.api.authenticate_by_key('TESTAPIKEY')
+        self.api.authenticate_by_key(self.api_key)
         self.api.conn.request('GET', '/current_customer')
 
     def test_auth_session_success(self):
         self.api.deauthenticate()
-        self.api.authenticate_by_password('foo@example.com', 'password')
+        self.api.authenticate_by_password(self.user, self.password)
         self.api.conn.request('GET', '/current_customer')
 
 if __name__ == '__main__':
