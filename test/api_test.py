d51198da4b97c1237fc797d7e53027c74e0dcbe2
David Gunter <david.gunter@tivix.com>
2018-06-25 09:45:41 -0700

    Bring back (deprecated) endpoint tests for minor version update (#33)

--- test/api_test.py
+++ test/api_test.py
@@ -3,6 +3,7 @@ import os
 from fastly import fastly, errors
 from fastly._version import __version__
 
+
 class APITest(unittest.TestCase):
 
     def setUp(self):
@@ -25,6 +26,12 @@ class APITest(unittest.TestCase):
         self.api.authenticate_by_key(self.api_key)
         self.assertTrue(self.api.purge_key(self.service_id, 'foo'))
 
+    def test_cookie_purge_by_key(self):
+        self.api.deauthenticate()
+        self.api.authenticate_by_password(self.user, self.password)
+        with self.assertRaises(fastly.AuthenticationError):
+            self.api.purge_key(self.service_id, 'foo')
+
     def test_soft_purge(self):
         self.api.deauthenticate()
         self.assertTrue(self.api.soft_purge_url(self.host, '/'))
@@ -34,10 +41,21 @@ class APITest(unittest.TestCase):
         self.api.authenticate_by_key(self.api_key)
         self.assertTrue(self.api.soft_purge_key(self.service_id, 'foo'))
 
+    def test_cookie_soft_purge_by_key(self):
+        self.api.deauthenticate()
+        self.api.authenticate_by_password(self.user, self.password)
+        with self.assertRaises(fastly.AuthenticationError):
+            self.api.soft_purge_key(self.service_id, 'foo')
+
     def test_auth_error(self):
         self.api.deauthenticate()
         with self.assertRaises(errors.AuthenticationError):
             self.api.conn.request('GET', '/current_customer')
+
+    def test_auth_session_success(self):
+        self.api.deauthenticate()
+        self.api.authenticate_by_password(self.user, self.password)
+        self.api.conn.request('GET', '/current_customer')
 
     def test_auth_key_success(self):
         self.api.deauthenticate()
@@ -47,6 +65,7 @@ class APITest(unittest.TestCase):
     def test_sets_default_user_agent_header(self):
         default_headers = self.api.conn.default_headers
         self.assertEqual(default_headers['User-Agent'], 'fastly-py-v{}'.format(__version__))
+
 
 if __name__ == '__main__':
     unittest.main()
