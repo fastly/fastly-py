b7c17590274023b1c198474ad82607c4cd639bb7
Katsuyuki Omuro <komuro@fastly.com>
2022-04-05 16:24:16 +0900

    New build based on updates

--- fastly/apis/__init__.py
+++ fastly/apis/__init__.py
@@ -6,7 +6,7 @@
 # raise a `RecursionError`.
 # In order to avoid this, import only the API that you directly need like:
 #
-#   from .api.acl_api import AclApi
+#   from fastly.api.acl_api import AclApi
 #
 # or import this package, but before doing it, use:
 #
